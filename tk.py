9d651d7df875e004593955ce8bf294b50ef0212b
Patrick McNally <Patrick.J.McNally@gmail.com>
2018-08-23 18:57:16 -0500

    Move links to class level variable

--- tk.py
+++ tk.py
@@ -3,6 +3,14 @@ import webbrowser as wb
 
 
 class Ticker(tk.Frame):
+    links = {
+        "zacks": "https://www.zacks.com/stock/quote/{0}",
+        "finviz": "https://finviz.com/quote.ashx?t={0}",
+        "openinsider": "http://openinsider.com/search?q={0}",
+        "earningswhispers": "https://earningswhispers.com/stocks/{0}",
+        "shortsqueeze": "http://shortsqueeze.com/?symbol={0}"
+    }
+
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
@@ -26,19 +34,8 @@ class Ticker(tk.Frame):
     def openall(self, event=None):
         symbol = self.text_box.get()
 
-        links = {
-            "zacks": "https://www.zacks.com/stock/quote/{0}",
-            "finviz": "https://finviz.com/quote.ashx?t={0}",
-            "openinsider": "http://openinsider.com/search?q={0}",
-            "earningswhispers": "https://earningswhispers.com/stocks/{0}",
-            "shortsqueeze":
-                "http://shortsqueeze.com/?symbol={0}&submit=Short+Quote"
-        }
-
-        for link in links.values():
+        for link in self.links.values():
             wb.open(link.format(symbol))
-        self.text_box.delete(0, 'end')
-        self.text_box.focus()
 
 
 root = tk.Tk()
